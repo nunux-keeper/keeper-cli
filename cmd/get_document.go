96853e2d62c033200e5a5c3df992b8a28782d3ea
Nicolas Carlier <n.carlier@nunux.org>
2016-08-20 17:52:44 +0200

    feat(document): Add attribute selection flag.

--- cmd/get_document.go
+++ cmd/get_document.go
@@ -2,16 +2,19 @@ package cmd
 
 import (
 	"errors"
+	"fmt"
 	"io"
+	"reflect"
 	"text/template"
 
 	"github.com/spf13/cobra"
 
+	"github.com/ncarlier/keeper-cli/api"
 	cmdutil "github.com/ncarlier/keeper-cli/cmd/util"
 )
 
 var DocumentTmpl = `Document:
- ID:          {{.Id}}
+ Id:          {{.Id}}
  Title:       {{.Title}}
  ContentType: {{.ContentType}}
  Content:     {{.Content}}
@@ -20,7 +23,18 @@ var DocumentTmpl = `Document:
  Ghost:       {{.Ghost}}
 `
 
+type getOptions struct {
+	attribute string
+}
+
+func getField(d *api.DocumentResponse, field string) string {
+	r := reflect.ValueOf(d)
+	f := reflect.Indirect(r).FieldByName(field)
+	return f.String()
+}
+
 func NewCmdGetDocument(f *cmdutil.Factory, out io.Writer) *cobra.Command {
+	var opts getOptions
 	cmd := &cobra.Command{
 		Use:   "get (ID)",
 		Short: "Get a document",
@@ -30,14 +44,18 @@ func NewCmdGetDocument(f *cmdutil.Factory, out io.Writer) *cobra.Command {
 			}
 			docid := args[0]
 
-			return runGetDocument(f, out, cmd, docid)
+			return runGetDocument(f, out, cmd, docid, &opts)
 		},
 		ValidArgs: []string{"create", "rm", "restore", "destroy"},
 	}
+
+	flags := cmd.Flags()
+	flags.StringVarP(&opts.attribute, "attr", "a", "", "Attribute selection")
+
 	return cmd
 }
 
-func runGetDocument(f *cmdutil.Factory, out io.Writer, cmd *cobra.Command, docid string) error {
+func runGetDocument(f *cmdutil.Factory, out io.Writer, cmd *cobra.Command, docid string, opts *getOptions) error {
 	c, err := f.Client()
 	if err != nil {
 		return err
@@ -46,6 +64,11 @@ func runGetDocument(f *cmdutil.Factory, out io.Writer, cmd *cobra.Command, docid
 	document, err := c.GetDocument(docid)
 	if err != nil {
 		return err
+	}
+
+	if opts.attribute != "" {
+		fmt.Fprintln(out, getField(document, opts.attribute))
+		return nil
 	}
 
 	tmpl, err := template.New("document").Parse(DocumentTmpl)
