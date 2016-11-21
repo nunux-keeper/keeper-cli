e8b93e9cb419fd45d5dc9c2d848b8b31b3a21c6a
Nicolas Carlier <n.carlier@nunux.org>
2016-11-21 20:51:32 +0100

    chore(): Update package URL.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-release_url="https://api.github.com/repos/ncarlier/keeper-cli/releases/latest"
+release_url="https://api.github.com/repos/nunux-keeper/keeper-cli/releases/latest"
 download_url=`curl -s $release_url | grep browser_download_url | head -n 1 | cut -d '"' -f 4`
 
 sudo curl -o /usr/local/bin/keepctl -L $download_url
